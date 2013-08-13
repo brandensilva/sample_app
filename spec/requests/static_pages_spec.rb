9fcec6619b9b94c80302d94b24348064944ce07f
Branden Silva <qksilva@gmail.com>
2013-08-13 11:10:25 -0600

    cleaned up specs

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -37,4 +37,25 @@ describe "Static pages" do
 		it { should have_content('Contact') }
 		it { should have_title("#{base_title} | Contact") } 
 	end
+
+	it 'should have the right links on the layout' do
+		visit root_path
+		click_link "About"
+		expect(page).to have_title('About Us')
+
+		click_link "Help"
+		expect(page).to have_title ('Help')
+
+		click_link "Contact"
+		expect(page).to have_title('Contact')
+	
+		click_link "Home"
+	
+		click_link "Sign up now!"
+		expect(page).to have_title('Sign up')
+	
+		click_link "sample app"
+		expect(page).to have_content('Sample App')
+	end
+
 end
